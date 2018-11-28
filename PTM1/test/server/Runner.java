f4ece4e78dba4a08a16115da1866beb2034ccc7d
esterhatuel <ester.hatchuel@gmail.com>
2018-11-28 05:56:48 +0200

    adding Runner

--- PTM1/test/server/Runner.java
+++ PTM1/test/server/Runner.java
@@ -1,5 +1,20 @@
 package server;
 
 public class Runner {
-
+    // run your server here
+    static Server s;
+    public static void runServer(int port){
+        s=new MyServer(port);
+        s.start(new MyClientHandler());
+    }
+    // stop your server here
+    public static void stopServer(){
+        s.stop();
+    }
+    public static void main(String[] args) {
+        int port=6400;
+        runServer(port);
+        //stopServer();
+        System.out.println("done");
+    }
 }
